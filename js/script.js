4ba5d6d29ae197ee1b50df0ebfdd8b7b56efbd61
Andrezza Souza <andrezzasouza@id.uff.br>
2021-07-30 08:40:23 -0300

    Acrescentou cartas ao tabuleiro

--- js/script.js
+++ js/script.js
@@ -1,5 +1,19 @@
 const amountCards = Number(prompt("Com quantas cartas você quer jogar? Escolha um número par de 4 a 14."));
 
+function insertCards(amountCards) {
+    const area = document.querySelector(".game-area");
+    let insert = "";
+
+
+    for (let i = 0; i < amountCards; i++) {
+        insert += `<div class="card-back" onclick="flipCard();"><img src="/img/front.png" /></div>`;
+    }
+
+    area.innerHTML = insert;
+}
+
+insertCards(amountCards);
+
 
 
 /* HTML da carta de costas */
