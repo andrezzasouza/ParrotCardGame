10949265d11a5a9f96bdc921b3ff6e8d2a00f260
Andrezza Souza <andrezzasouza@id.uff.br>
2021-07-30 09:07:56 -0300

    Impediu algumas entradas no prompt

--- js/script.js
+++ js/script.js
@@ -1,4 +1,8 @@
-const amountCards = Number(prompt("Com quantas cartas você quer jogar? Escolha um número par de 4 a 14."));
+let amountCards = Number(prompt("Com quantas cartas você quer jogar? Escolha um número par de 4 a 14."));
+
+while (amountCards < 4 || amountCards > 14 || amountCards % 2 !== 0) {
+    amountCards = Number(prompt("Com quantas cartas você quer jogar? Escolha um número par de 4 a 14."));
+}
 
 function insertCards(amountCards) {
     const area = document.querySelector(".game-area");
@@ -6,13 +10,24 @@ function insertCards(amountCards) {
 
 
     for (let i = 0; i < amountCards; i++) {
-        insert += `<div class="card-back" onclick="flipCard();"><img src="/img/front.png" /></div>`;
+        insert += `<div class="card-back" onclick="flipCard(this);">
+                        <img src="/img/front.png" />
+                    </div>`;
     }
 
     area.innerHTML = insert;
 }
 
 insertCards(amountCards);
+
+let playCounter = 0;
+
+function flipCard(clickedCard) {
+    // instruções para virar a carta
+    playCounter++
+}
+
+
 
 
 
