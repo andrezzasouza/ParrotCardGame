1594d5812f95f6c71785fc754416387eacbd3c28
Andrezza Souza <andrezzasouza@id.uff.br>
2021-08-01 21:31:54 -0300

    Inseriu contador de tempo no jogo

--- js/script.js
+++ js/script.js
@@ -1,3 +1,5 @@
+let start = true;
+
 // CARREGA PROMPT NO INÍCIO DO JOGO
 
 let amountCards = Number(prompt("Com quantas cartas você quer jogar? Escolha um número par de 4 a 14."));
@@ -14,7 +16,7 @@ function compare() {
     return Math.random() - 0.5;
 }
 
-// INSERE AS CARTAS NO TABULEIRO
+// INSERE AS CARTAS NO TABULEIRO E RELÓGIO NA TELA
 
 function insertCards(amountCards) {
 
@@ -25,8 +27,6 @@ function insertCards(amountCards) {
     for (let i = 0; i < (amountCards / 2); i++) {
         duplicateArray.push(picArray[i]);
         duplicateArray.push(picArray[i]);
-
-        //posso comparar o número do índice da carta pra saber se elas são iguais ou não
     }
 
     duplicateArray.sort(compare);
@@ -45,16 +45,20 @@ function insertCards(amountCards) {
                     </div>`;
 
     }
-
     area.innerHTML = insert;
 }
 
+let addClock = document.querySelector(".counter")
+
+function insertClock() {
+    addClock.innerHTML = "0<span>s</span>";
+    addClock.classList.add("show-clock");
+}
+
+insertClock();
 insertCards(amountCards);
 
-
-
 let amountPairs = amountCards / 2;
-
 
 let showFront = "";
 let hideBack = "";
@@ -63,37 +67,34 @@ let firstCard = "";
 let secondCard = "";
 
 let playCounter = 0;
+let time = 0;
 
 const board = document.querySelector(".game-area");
+let idInterval;
+
+// INICIA CONTAGEM DO RELÓGIO
+
+const startClock = function () {
+    if (amountPairs === 0) {
+        clearInterval(idInterval);
+    } else {
+        time++;
+        addClock.innerHTML = `${time}<span>s</span>`;
+    }
+}
+
+// VIRA AS CARTAS
 
 function flipCard(clickedCard) {
-
-    console.log(clickedCard)
-    console.log ("mostra valor do clickedCard")
-
-    // const parentElement = clickedCard.parentNode;
-
-    // console.log(parentElement);
-    // console.log("parentElement aqui em cima");
-
-    
+    if (playCounter === 0) {
+        idInterval = setInterval(startClock, 1000);
+    }
 
     showFront = clickedCard.querySelector(".back-face");
-    console.log(showFront);
-    console.log("showFront antes do remove");
     showFront.classList.remove("effect-back");
-    console.log(showFront);
-    console.log("showFront depois do remove");
 
     hideBack = clickedCard.querySelector(".effect-front");
-    console.log(hideBack);
-    console.log("hideBack antes do remove");
     hideBack.classList.remove("effect-front");
-    console.log(hideBack);
-    console.log("hideBack depois do remove");
-
-    console.log(playCounter);
-    console.log("mostra valor do playCounter")
 
     if (playCounter % 2 === 0) {
         firstCard = hideBack;
@@ -111,9 +112,10 @@ function flipCard(clickedCard) {
             setTimeout(allowClicks, 1000);
         }
     }
-
     playCounter++;    
 }
+
+// DESVIRA AS CARTAS
 
 const unturnCards = function () {
 
@@ -129,10 +131,11 @@ const unturnCards = function () {
     secondDivList[1].classList.add("effect-front");
 }
 
+// VOLTA A PERMITIR CLIQUES DEPOIS DE VIRAR 2 CARTAS
+
 const allowClicks = function () {
     board.classList.remove("no-clicking");
 }
-
 
 // FINALIZA O JOGO
 
@@ -143,5 +146,46 @@ function checkEndGame () {
 }
 
 const delayedEnding = function () {
-    alert(`Você ganhou o jogo em ${playCounter} jogadas!`);
+    alert(`Você ganhou o jogo em ${playCounter} jogadas e ${time} segundos!`);
+    restartGame();
 }
+
+// VERIFICA SE JOGO RECOMEÇA
+
+let restart = "";
+
+function restartGame() {
+
+    let validAnswer = false;
+
+    do {
+        restart = prompt("Você quer recomeçar? S ou N?");
+        if (restart === "S" || restart === "N") {
+            validAnswer = true;
+        }
+    } while (validAnswer !== true);
+
+    const clearGame = document.querySelectorAll(".card");
+
+    for (let i = 0; i < clearGame.length; i++) {
+        clearGame[i].remove();  
+    }
+
+    if (restart === "S") {
+        const screen = document.querySelector("body");
+        screen.innerHTML = "";
+        start = true;
+        clearInterval(idInterval);
+
+    } else if (restart === "N") {
+        for (let i = 0; i < clearGame.length; i++) {
+            clearGame[i].remove();  
+        }
+        const overMessage = document.querySelector("h1");
+        overMessage.innerHTML = "GAME OVER!"
+        clearInterval(idInterval);
+        start = false;
+    }
+}
+
+
